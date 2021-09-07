8b5e3c97fb08a4c57bf9d85a6be884b0967c9e3c
이지원 <gbwlxhd97@naver.com>
2021-09-07 18:00:27 +0900

    create slider && slideItem component

--- src/app/module/common/slider/component/slider-item/slider-item.component.ts
+++ src/app/module/common/slider/component/slider-item/slider-item.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slider-item',
   templateUrl: './slider-item.component.html',
-  styleUrls: ['./slider-item.component.scss']
+  styleUrls: ['./slider-item.component.scss'],
 })
 export class SliderItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    for (let i = 1; i < 13; i++) {
+      this.date12.push(i);
+    }
+  }
+  today = new Date();
+  x = 0;
+  date12: any = [];
+  ngOnInit(): void {}
+  prev_month() {
+    if (this.x >= 0) {
+      this.x = this.x + 80;
+    }
+    if (this.x <= 0) {
+      this.x += 80;
+    }
+    if (this.x == 80) {
+      this.x = -880;
+    }
+    if (this.x == -880) {
+      this.x = -880;
+    }
+    console.log(this.x);
   }
 
+  next_month() {
+    if (this.x <= 0) {
+      this.x = this.x - 80;
+    }
+    if (this.x >= 0) {
+      this.x -= 80;
+    }
+
+    if (this.x == -960) {
+      this.x = 0;
+    }
+    console.log(this.x);
+  }
 }
--- src/app/module/common/slider/component/slider/slider.component.ts
+++ src/app/module/common/slider/component/slider/slider.component.ts
@@ -3,13 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
+  constructor() {
+    for (let i = 1; i < 13; i++) {
+      let data = this.date12.push('◎' + i);
+      this.roundI = data;
+    }
+  }
+  roundI: any;
+  today = new Date();
+  x = -0;
+  click = false;
+  date12: any = [];
+  sun: any;
+  sat: any;
+  targetDate: any;
+  dates: any = []; //일자
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  clickRound() {
+    this.x = 0;
   }
 
+  prev_slide() {
+    if (this.x >= 0) {
+      this.x = this.x + 80;
+    }
+    if (this.x <= 0) {
+      this.x += 80;
+    }
+    if (this.x == 80) {
+      this.x = -880;
+    }
+    if (this.x == -880) {
+      this.x = -880;
+    }
+    console.log(this.x);
+  }
+
+  next_slide() {
+    if (this.x <= 0) {
+      this.x = this.x - 80;
+    }
+    if (this.x >= 0) {
+      this.x -= 80;
+    }
+
+    if (this.x == -960) {
+      this.x = 0;
+    }
+    console.log(this.x);
+  }
 }
